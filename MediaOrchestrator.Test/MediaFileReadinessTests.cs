8a639270d3bc8bf29cc50be099eb0131ba3f80f5
Андрей <andrej@MacBook-Pro-Andrej.local>
2026-03-29 12:20:59 +0500

    fix macos internal test

--- MediaOrchestrator.Test/MediaFileReadinessTests.cs
+++ MediaOrchestrator.Test/MediaFileReadinessTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using MediaOrchestrator.Exceptions;
 using Xunit;
@@ -38,23 +39,38 @@ namespace MediaOrchestrator.Test
             var path = Path.Combine(directory, "growing.mp4");
             File.WriteAllBytes(path, new byte[] { 0x00 });
 
+            using var writerCancellation = new CancellationTokenSource();
+            var firstGrowthObserved = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
             var writer = Task.Run(async () =>
             {
-                for (var i = 0; i < 8; i++)
+                while (!writerCancellation.Token.IsCancellationRequested)
                 {
-                    await File.AppendAllTextAsync(path, "x").ConfigureAwait(false);
-                    await Task.Delay(20).ConfigureAwait(false);
+                    using (var stream = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+                    {
+                        await stream.WriteAsync(new byte[] { 0x01 }, 0, 1, writerCancellation.Token).ConfigureAwait(false);
+                        await stream.FlushAsync(writerCancellation.Token).ConfigureAwait(false);
+                    }
+
+                    if (new FileInfo(path).Length > 1)
+                    {
+                        firstGrowthObserved.TrySetResult(true);
+                    }
+
+                    await Task.Delay(1, writerCancellation.Token).ConfigureAwait(false);
                 }
-            });
+            }, writerCancellation.Token);
+
+            await firstGrowthObserved.Task.ConfigureAwait(false);
 
             var exception = await Assert.ThrowsAsync<InputFileStillBeingWrittenException>(() =>
                 MediaFileReadiness.WaitUntilStableAsync(
                     path,
-                    stabilityQuietPeriod: TimeSpan.FromMilliseconds(50),
-                    pollInterval: TimeSpan.FromMilliseconds(10),
-                    maximumWait: TimeSpan.FromMilliseconds(120))).ConfigureAwait(false);
+                    stabilityQuietPeriod: TimeSpan.FromMilliseconds(25),
+                    pollInterval: TimeSpan.FromMilliseconds(2),
+                    maximumWait: TimeSpan.FromMilliseconds(200))).ConfigureAwait(false);
 
-            await writer.ConfigureAwait(false);
+            writerCancellation.Cancel();
+            await Assert.ThrowsAnyAsync<OperationCanceledException>(async () => await writer.ConfigureAwait(false)).ConfigureAwait(false);
             Assert.Contains(path, exception.Message);
         }
     }
